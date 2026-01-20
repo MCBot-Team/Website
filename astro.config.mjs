bd89cfd38d035dbaa6b254707a2e9019dd29f876
Rezn1r <rezn1r@proton.me>
2026-01-20 17:51:50 +0800

    fix: put compress in initialize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-
 import playformCompress from "@playform/compress";
 
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
   imageService: "compile",
-  integrations: [playformCompress()],
-});
-
-playformCompress({
-  CSS: true,
-  HTML: true,
-  Image: false,
-  JavaScript: true,
-  SVG: true,
+  integrations: [
+    playformCompress({
+      CSS: true,
+      HTML: true,
+      Image: false,
+      JavaScript: true,
+      SVG: true,
+    }),
+  ],
 });
