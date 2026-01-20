8d4ab9ccd94eb159a89330365c955790b93f9d1c
Rezn1r <rezn1r@proton.me>
2026-01-20 17:04:24 +0800

    feat: add @playform/compress and config for it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
+import playformCompress from "@playform/compress";
+
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  imageService: "cloudflare",
+  imageService: "compile",
+  integrations: [playformCompress()],
+});
+
+playformCompress({
+  CSS: true,
+  HTML: true,
+  Image: false,
+  JavaScript: true,
+  SVG: true,
 });
